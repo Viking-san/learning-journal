09b6e4b66f5399db7f1bddb5c1a44eb32a15e9d6
Pans <caxapoker@gmail.com>
2026-02-24 13:19:39 +0700

    Fix CSRF_TRUSTED_ORIGINS for empty strings

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
+CSRF_TRUSTED_ORIGINS = [x for x in config('CSRF_TRUSTED_ORIGINS', default='').split(',') if x]
 
 # Application definition
 
