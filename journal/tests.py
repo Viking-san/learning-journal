aba17e0d6f716b672a919959aaa1d987d51c36d8
Pans <caxapoker@gmail.com>
2026-03-18 17:59:05 +0700

    tests for EntryForm

--- journal/tests.py
+++ journal/tests.py
@@ -5,6 +5,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from .models import Category, Entry, Tag, Comment, EntryLog
 from .views import EntryListView, DraftListView
+from .forms import EntryForm, CommentForm
 from django.db.utils import IntegrityError
 from pprint import pprint
 
@@ -485,6 +486,60 @@ class SignalsTest(TestCase):
         self.assertEqual(log.changed_entry_id, entry_id)
 
 
+class EntryFormTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test category')
+    def test_form_is_valid(self):
+        
+        form_data = {
+            'title': 'Test Entry',
+            'content': 'text',
+            'category': self.category,
+            'is_published': True
+        }
+        form = EntryForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(Entry.objects.count(), 1)
+
+    def test_form_without_required_fields(self):
+        form_data = {
+            'title': 'Test Entry',
+            'content': 'text'
+        }
+        form = EntryForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_form_defaults(self):
+        form_data = {
+            'title': 'test_form_defaults',
+            'content': 'text',
+            'category': self.category,
+        }
+        form = EntryForm(data=form_data)
+
+        self.assertTrue(form.is_valid())        
+        form.save()
+        # В модели Entry по умолчанию is_published=True,
+        # но при создании через форму, если checkbox не активен, 
+        # а он не активен если не передать явно, то он будет False
+        self.assertFalse(Entry.objects.get(title='test_form_defaults').is_published)
+
+    def test_form_tags(self):
+        tag = Tag.objects.create(name='Test Tag')
+        form_data = {
+            'title': 'test_form_tags',
+            'content': 'text',
+            'category': self.category,
+            'tags': [tag]
+        }
+        form = EntryForm(data=form_data)
+
+        self.assertTrue(form.is_valid())        
+        form.save()
+        entry_tags = Entry.objects.get(title='test_form_tags').tags.all()
+        self.assertIn(tag, entry_tags)
 
 
 # class EntryAPITest(APITestCase):
