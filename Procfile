5a8e57260e5d9ea330f8d499d2714455aecf8617
Pans <caxapoker@gmail.com>
2026-04-07 15:11:23 +0700

    remove local files don't make sense now

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn config.wsgi:application --log-file -
-release: python manage.py migrate --noinput
--- rebuild_local_container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/pans/projects/learning-journal
-docker-compose -f docker-compose.local.yml down
-docker-compose -f docker-compose.local.yml up -d --build
